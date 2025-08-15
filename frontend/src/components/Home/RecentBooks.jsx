c0f879fcbc555797e41b0b5325aaf21f418834d5
PuskarMal <puskarmal07@gmail.com>
2025-08-15 19:22:09 +0530

    Update RecentBooks.jsx

--- frontend/src/components/Home/RecentBooks.jsx
+++ frontend/src/components/Home/RecentBooks.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import BookCard from "../BookCard/BookCard";
 import Loader from "../Loader/Loader";
+import TopRatedBooks froms "./TopRatedBooks"
 import axios from "axios";
 const RecentBooks = () => {
   const [Data, setData] = useState();
@@ -17,9 +18,7 @@ const RecentBooks = () => {
   <div className="mt-16 px-4">
     <h4 className="text-3xl text-yellow-100">New Arrivals</h4>
     { !Data && (
-    <div className="flex items-center justify-center my-8">
-      <Loader />{" "}
-    </div>)}
+    <TopRatedBooks/>
     <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 gap-8">
       {Data && Data.map((items,i) => (
       <div key={i} className="shadow-lg rounded-lg flex text-center justify-center">
