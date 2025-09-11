73b5f3ba1ee2c65a1bbef5d17b5e9aa9771b868d
PuskarMal <puskarmal07@gmail.com>
2025-09-11 20:20:10 +0530

    Update RecentBooks.jsx

--- frontend/src/components/Home/RecentBooks.jsx
+++ frontend/src/components/Home/RecentBooks.jsx
@@ -1,45 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from "react";
+import BookCard from "../BookCard/BookCard";
 import Loader from "../Loader/Loader";
 import axios from "axios";
-import BookCard from "../BookCard/BookCard";
-
 const RecentBooks = () => {
-  const [Data, setData] = useState(null); // initial null
-
-  useEffect(() => {
-    const fetchRecentBooks = async () => {
-      try {
-        const response = await axios.get(`https://librarybackend-3-73l4.onrender.com/api/auth/get-recentbooks`);
-        setData(response.data); // 💡 this was missing!
-        
-      } catch (error) {
-        console.error("Error fetching recommendations", error);
-      }
-    };
-
-    fetchRecentBooks();
-  }, []);
-
-  return (
-  <div className="mt-16 ">
+  const [Data, setData] = useState();
+  useEffect(() => { 
     
-    {!Data ? (
-      <div></div>
-    ) : (
-      <div>
-        <h4 className="text-3xl text-yellow-100 mb-6">New Arrivals</h4>
-        <div className="my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8">
-          {Data.map((item, i) => (
-            <div key={i} className="shadow-lg rounded-lg flex justify-center">
-              <BookCard data={item} />
-            </div>
-          ))}
-        </div>
+    const fetch = async () => {
+      const response = await axios.get("https://librarybackend-3-73l4.onrender.com/api/auth/get-recentbooks");
+      setData(response.data.data);
+    }
+    fetch();
+  }, []);
+  
+  return (
+  <div className="mt-16 px-4">
+    <h4 className="text-3xl text-yellow-100">New Arrivals</h4>
+    { !Data && (
+    <div className="flex items-center justify-center my-8">
+      <Loader />{" "}
+    </div>)}
+    <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 gap-8">
+      {Data && Data.map((items,i) => (
+      <div key={i} className="shadow-lg rounded-lg flex text-center justify-center">
+        <BookCard data={items} />{" "} 
       </div>
-    )}
+    ))}
+    </div>
   </div>
-);
-
+  );
 };
 
 export default RecentBooks;
