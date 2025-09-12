9459e727b19ac8fabf8b36d665b430faa431578f
PuskarMal <puskarmal07@gmail.com>
2025-09-12 12:24:44 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,12 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from "react-redux"
 import store from './store/index.js'
+import ScrollToTop from './scrollToTop';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop/>
       <Provider store={store}>
         <GoogleOAuthProvider clientId ="836764775589-17arug9qv0nq5dga5npdjoc71gjcpqcn.apps.googleusercontent.com">
         <App />
